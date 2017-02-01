f7909e520e1e24e5abaf581347a70f3028e578b7
steveraaen <sraaen@gmail.com>
2017-02-01 17:29:49 -0500

    Major changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 // Render the category buttons
     $('#list').on('click', function() {
         for (let i = 0; i < kits.length; i++) {
-            btn = $('<input id = "' + kits[i][0].kitName + '" class = "kit-btn" type="button" data-inline="true" value = "' + kits[i][0].kitName + '">' + kits[i][0].icon + '</input>')
+            btn = $('<input id = "' + kits[i][0].kitName + '" class = "kit-btn" type="button" data-inline="true" data-mini="true" value = "' + kits[i][0].kitName + '"    >' + kits[i][0].icon + '</input>')
                 /*btn.append(kits[i][0].kitName);*/
             $('#buttons').append(btn);
         }
@@ -12,19 +12,17 @@ $(document).ready(function() {
 
     });
 //===================================================================
-// 
+// Render lists of items required for each kit
     $(document).on('click', '#Evac', function() {
-
         for (let i = 1; i < goKit.length; i++) {
-            console.log("go" + goKit[i].icon)
             $('#content1').append('<div class="ui-block-a"><span>' + goKit[i].need + 
                 '</span></div><div class="ui-block-b"><span>' + goKit[i].icon +  
                 '</span></div><div class="ui-block-c"><span>' + iInfo +  
-                '</span></div><div class="ui-block-d"><span>' + iWarn +'</span></div>')
+                '</span></div>')
         }
-        $( "#t1" ).table( "rebuild" );
+
     })
-    //-----------
+    //------------------------------
         $(document).on('click', '#Stay', function() {
         	console.log("stay")
         for (let i = 1; i < stayKit.length; i++) {
@@ -38,12 +36,13 @@ $(document).ready(function() {
         }
     })
          $(document).on('click', '#Pet', function() {
-         	console.log("senior")
+         	console.log("pet")
         for (let i = 1; i < petKit.length; i++) {
             $('#l4').append('<li>' + petKit[i].need + '</li>')
         }
     })
              $(document).on('click', '#Car', function() {
+                console.log("car")
         for (let i = 1; i < carKit.length; i++) {
             $('#l5').append('<li>' + carKit[i].need + '</li>')
         }
--- assets/javascript/prep.js
+++ assets/javascript/prep.js
@@ -1,50 +1,50 @@
-   var iBank =  '<i class="fa fa-university" aria-hidden= "true"></i>';
-   var iBattery = '<i class="fa fa-battery-half" aria-hidden="true"></i>';
-   var iBed = '<i class="fa fa-bed" aria-hidden="true"></i>'
-   var iBike = '<i class="fa fa-bicycle" aria-hidden="true"></i>'
-   var iCar =  '<i class="fa fa-car" aria-hidden= "true"></i>';
-   var iChild =  '<i class="fa fa-child" aria-hidden= "true"></i>';
-   var iCredit =  '<i class="fa fa-credit-card" aria-hidden="true"></i>';
-   var iFemale =  '<i class="fa fa-venus" aria-hidden= "true"></i>';
-   var iFire =  '<i class="fa fa-fire" aria-hidden= "true"></i>';
-   var iFireEx =  '<i class="fa fa-fire-extinguisher" aria-hidden= "true"></i>';
-   var iFood = '<i class="fa fa-cutlery" aria-hidden="true"></i>';
-   var iHome = '<i class="fa fa-home" aria-hidden="true"></i>'
-   var iHeart =  '<i class="fa fa-heart" aria-hidden= "true"></i>';
-   var iIdCard =  '<i class="fa fa-id-card-o" aria-hidden= "true"></i>';
-   var iInfo =  '<i class="fa fa-info" aria-hidden= "true"></i>';
-   var iKey =  '<i class="fa fa-key" aria-hidden= "true"></i>';
-   var iLight = '<i class="fa fa-lightbulb-o" aria-hidden="true"></i>';
-   var iMale =  '<i class="fa fa-mars" aria-hidden= "true"></i>';
-   var iMap =  '<i class="fa fa-map-o" aria-hidden= "true"></i>';
-   var iMedkit =  '<i class="fa fa-medkit" aria-hidden= "true"></i>';
-   var iMoney =  '<i class="fa fa-money" aria-hidden="true"></i>';
-   var iMug = '<i class="fa fa-beer" aria-hidden="true"></i>'
-   var iPaw =  '<i class="fa fa-paw" aria-hidden= "true"></i>';
-   var iPencil = '<i class="fa fa-pencil" aria-hidden="true"></i>'
-   var iPhone = '<i class="fa fa-phone" aria-hidden="true"></i>'
-   var iPhoto = '<i class="fa fa-picture-o" aria-hidden="true"></i>'
-   var iPlace = '<i class="fa fa-map-marker" aria-hidden="true"></i>'
-   var iRadio = '<i class="fa fa-wifi" aria-hidden="true"></i>';
-   var iSenior =  '<i class="fa fa-wheelchair-alt" aria-hidden= "true"></i>';
-   var iShop =  '<i class="fa fa-shopping-cart" aria-hidden= "true"></i>';
-   var iSpoon =  '<i class="fa fa-spoon" aria-hidden= "true"></i>';
-   var iTech = '<i class="fa fa-usb" aria-hidden="true"></i>'
-   var iTrain =  '<i class="fa fa-train" aria-hidden= "true"></i>';
-   var iUmbrella = '<i class="fa fa-umbrella" aria-hidden="true"></i>'
-   var iUnk = '<i class="fa fa-window-close" aria-hidden="true"></i>'
-   var iUserCirc =  '<i class="fa fa-user-circle" aria-hidden= "true"></i>';
-   var iWarn = '<i class="fa fa-exclamation" aria-hidden="true"></i>'
-   var iWater = '<i class="fa fa-tint" aria-hidden="true"></i>';
 
+var iBank =  '<i class="fa fa-university" aria-hidden= "true"></i>';
+var iBattery = '<i class="fa fa-battery-half" aria-hidden="true"></i>';
+var iBed = '<i class="fa fa-bed" aria-hidden="true"></i>'
+var iBike = '<i class="fa fa-bicycle" aria-hidden="true"></i>'
+var iCar =  '<i class="fa fa-car" aria-hidden= "true"></i>';
+var iChild =  '<i class="fa fa-child" aria-hidden= "true"></i>';
+var iCredit =  '<i class="fa fa-credit-card" aria-hidden="true"></i>';
+var iFemale =  '<i class="fa fa-venus" aria-hidden= "true"></i>';
+var iFire =  '<i class="fa fa-fire" aria-hidden= "true"></i>';
+var iFireEx =  '<i class="fa fa-fire-extinguisher" aria-hidden= "true"></i>';
+var iFood = '<i class="fa fa-cutlery" aria-hidden="true"></i>';
+var iHome = '<i class="fa fa-home" aria-hidden="true"></i>'
+var iHeart =  '<i class="fa fa-heart" aria-hidden= "true"></i>';
+var iIdCard =  '<i class="fa fa-id-card-o" aria-hidden= "true"></i>';
+var iInfo =  '<i class="fa fa-info" aria-hidden= "true"></i>';
+var iKey =  '<i class="fa fa-key" aria-hidden= "true"></i>';
+var iLight = '<i class="fa fa-lightbulb-o" aria-hidden="true"></i>';
+var iMale =  '<i class="fa fa-mars" aria-hidden= "true"></i>';
+var iMap =  '<i class="fa fa-map-o" aria-hidden= "true"></i>';
+var iMedkit =  '<i class="fa fa-medkit" aria-hidden= "true"></i>';
+var iMoney =  '<i class="fa fa-money" aria-hidden="true"></i>';
+var iMug = '<i class="fa fa-beer" aria-hidden="true"></i>'
+var iPaw =  '<i class="fa fa-paw" aria-hidden= "true"></i>';
+var iPencil = '<i class="fa fa-pencil" aria-hidden="true"></i>'
+var iPhone = '<i class="fa fa-phone" aria-hidden="true"></i>'
+var iPhoto = '<i class="fa fa-picture-o" aria-hidden="true"></i>'
+var iPlace = '<i class="fa fa-map-marker" aria-hidden="true"></i>'
+var iRadio = '<i class="fa fa-wifi" aria-hidden="true"></i>';
+var iSenior =  '<i class="fa fa-wheelchair-alt" aria-hidden= "true"></i>';
+var iShop =  '<i class="fa fa-shopping-cart" aria-hidden= "true"></i>';
+var iSpoon =  '<i class="fa fa-spoon" aria-hidden= "true"></i>';
+var iTech = '<i class="fa fa-usb" aria-hidden="true"></i>'
+var iTrain =  '<i class="fa fa-train" aria-hidden= "true"></i>';
+var iUmbrella = '<i class="fa fa-umbrella" aria-hidden="true"></i>'
+var iUnk = '<i class="fa fa-window-close" aria-hidden="true"></i>'
+var iUserCirc =  '<i class="fa fa-user-circle" aria-hidden= "true"></i>';
+var iWarn = '<i class="fa fa-exclamation" aria-hidden="true"></i>'
+var iWater = '<i class="fa fa-tint" aria-hidden="true"></i>';
 
-
-   var icons = [iBank, iBed, iBattery, iBike, iCar, iChild, iCredit, iFemale, iFire, iFireEx, iFood, iHeart, iHome, iIdCard, iInfo, iKey, iLight, iMale, iMap, iMedkit, iMoney, iMug, iPaw, iPencil, iPhone, iPhoto, iPlace, iRadio, iSenior, iShop, iSpoon, iTech, iTrain, iUmbrella, iUnk, iUserCirc, iWarn, iWater]
+var icons = [iBank, iBed, iBattery, iBike, iCar, iChild, iCredit, iFemale, iFire, iFireEx, iFood, iHeart, iHome, iIdCard, iInfo, iKey, iLight, iMale, iMap, iMedkit, iMoney, iMug, iPaw, iPencil, iPhone, iPhoto, iPlace, iRadio, iSenior, iShop, iSpoon, iTech, iTrain, iUmbrella, iUnk, iUserCirc, iWarn, iWater]
 
 
 
 var goKit = [
 { 'kitName' : 'Evac',
+  'longName' : 'Evacuation Prep List',
   'icon' : iTrain
   },
 
@@ -53,7 +53,7 @@ var goKit = [
 		'goCat' : 'all',
 		'need' : 'Documents',
 		'descrip' : 'Copies of your important documents in a waterproof and portable container (insurance cards, birth certificates, deeds, photo IDs, proof of address, etc.)',
-		'icon' : iTrain
+		'icon' : iMap
 	},
 	{
 		'event' : ["All Events","Earthquake","Severe Drought","Wildfire","Flood","Hurricane","Blizzard","Heat Wave"],
@@ -155,6 +155,7 @@ var goKit = [
 	}]
 	var stayKit = [
 { 'kitName' : 'Stay',
+  'longName' : 'Shelter In Place',
   'icon' : iHome
   },
 	{
@@ -228,6 +229,7 @@ var goKit = [
 	}]
 var seniorKit = [
 { 'kitName' : 'Senior',
+  'longName' : 'Senior Citizen Needs',
   'icon' : iSenior
 },
 	{
@@ -260,6 +262,7 @@ var seniorKit = [
 	}]
 var petKit = [
 { 'kitName' : 'Pet',
+  'longName' : 'Protecting Pets',
   'icon' : iPaw
 },
 	{
@@ -362,6 +365,7 @@ var petKit = [
 
 var carKit = [
 { 'kitName' : 'Car',
+  'longName' : 'Vehicle Preparation',
   'icon'  : iCar
 },
 	{
@@ -436,3 +440,42 @@ var avgCal = [
     {"label":"Child Male", "value":1967},
     {"label":"Child Female", "value":1670}
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
